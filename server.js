df02df70479dfd0fba2a7b9c0200b3d097f58bbb
dregh1 <rakotonirinacharlesandrea@gmail.com>
2025-11-23 20:43:44 +0300

    Add server file contains socket

---
+++ server.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:8100',
+    methods: ['GET', 'POST'],
+  },
+});
+
+function getRoomSize(code) {
+  const room = io.sockets.adapter.rooms.get(code);
+  return room ? room.size : 0;
+}
+
+io.on('connection', (socket) => {
+  console.log('client connected', socket.id);
+
+  socket.on('room:create', ({ code }) => {
+    const size = getRoomSize(code);
+    if (size >= 2) {
+      socket.emit('room:error', { message: 'Room déjà complète' });
+      return;
+    }
+    socket.join(code);
+    const players = getRoomSize(code);
+    io.to(code).emit('room:status', { code, players });
+    if (players === 2) {
+      io.to(code).emit('room:ready', { code });
+    }
+  });
+
+  socket.on('room:join', ({ code }) => {
+    const size = getRoomSize(code);
+    if (size === 0) {
+      socket.emit('room:error', { message: 'Room introuvable' });
+      return;
+    }
+    if (size >= 2) {
+      socket.emit('room:error', { message: 'Room complète' });
+      return;
+    }
+    socket.join(code);
+    const players = getRoomSize(code);
+    io.to(code).emit('room:status', { code, players });
+    if (players === 2) {
+      io.to(code).emit('room:ready', { code });
+    }
+  });
+
+  socket.on('room:leave', ({ code }) => {
+    socket.leave(code);
+    const players = getRoomSize(code);
+    io.to(code).emit('room:status', { code, players });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('client disconnected', socket.id);
+  });
+});
+
+const PORT = 3001;
+server.listen(PORT, () => {
+  console.log('Socket.IO server on http://localhost:' + PORT);
+});
